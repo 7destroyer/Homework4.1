a70cf4b5fdbf80dfe20e32124dd5b8c7442dbc25
Anton <antoha357357@gmail.com>
2025-02-08 17:28:23 +0300

    Add project files.

---
+++ Homework4.1/Homework4.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Recursive(int a, int b);
+int Iterative(int a, int b);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int num1, num2;
+    cout << "Введите первое число: " << endl;
+    cin >> num1;
+    cout << "Введите второе число: " << endl;
+    cin >> num2;
+    int res1 = Recursive(num1, num2);
+    int res2 = Iterative(num1, num2);
+    cout << "НОД (рекурсивный): " << res1 << endl;
+    cout << "НОД (итерационный): " << res2 << endl;
+    return 0;
+}
+
+int Recursive(int a, int b) {
+    return (b == 0) ? a : Recursive(b, a % b);
+}
+
+int Iterative(int c, int d) {
+    while (d != 0) {
+        int temp = d;
+        d = c % d;
+        c = temp;
+    }
+    return c;
+}
